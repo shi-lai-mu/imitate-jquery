ccd51227e7f806f80e175c07314f7676817b7bc5
shi_lai_mu <478889187@qq.com>
2019-02-09 17:45:08 +0800

    去除注释

--- slm.js
+++ slm.js
@@ -19,72 +19,7 @@
  *       \|_______| \|_______| \|__|    \|__|          \|_______| \|_______| \|_________| \|_________|    *
  *                                                                                                        *
 \* ====================================================================================================== */
-/**
- * 接口API:
- *      $()
- *      $(Element Object / String)      // 元素节点选择器: ID选择器 | class选择器 | css选择器 | 标签选择器 | 标签创建器 "[可继续链式]"
- * 
- *      ID选择器    : $("#id")
- *      class选择器 : $(".class")
- *      css选择器   : $(".warp > div")
- *      标签选择器  : $("div")
- *      标签创建器  : $("<div>sss<span>max</span></div><span></span>")  创建标签并加入document子节点内 (需配合 _$(Element).copyElement(parent))
- * 
- *      
- *  -工具类：
- *      $.type(value)             // 返回数据类型
- *      $.each(obj, cb, self)     // 遍历对象
- *      $.select(string/element)  // 选择器
- * 
- *  -工具类：
- *      $.append(index[, value])  // 向匹配的元素内添加内容
- * 
- *  -属性类：
- *      $.val(index[, value])     // 设置value值
- *      $.css(pro, value)         // 设置css属性
- * 
- *  -选择类：
- *      $.eq(index)               // 正数/负数 选择其中一个
- *      $.first()                 // 选中第一个
- *      $.last()                  // 选中最后一个
- * 
- * 
- * 
- * 
- *  -文本操作：
- *      $(Element).html(html)           // 有实参则赋值所有元素的innerHTML | 没有实参返回所有元素的innerHTML"[不可链式]"
- *      $(Element).text(text)           // 有实参则赋值所有元素的innerText | 没有实参返回所有元素的innerText"[不可链式]"
- * 
- *  -事件操作：
- *      $(Element).addEvent(EventName, EventFunction)     // 为所有元素委托事件函数                       "[可继续链式]"
- *      $(Element).removeEvent(EventName, EventFunction)  // 移除所有元素委托事件函数                     "[可继续链式]"
- * 
- *  -类操作：
- *      $(Element).addClass(className)                    // 为所有元素添加类                             "[可继续链式]"
- *      $(Element).removeClass(className)                 // 为所有元素移除类                             "[可继续链式]"
- *      $(Element).toggleClass(className)                 // 为所有元素切换类                             "[可继续链式]"
- * 
- *  -节点操作:
- *      $(Element).append(Element)                        // 添加子节点                                  "[可继续链式]"
- *      $(Element).remove(Element)                        // 移除子节点                                  "[可继续链式]"
- *      $(Element).copyElement(parent)                    // 复制所有元素到指定的父节点内                  "[可继续链式]"
- * 
- *  -样式操作:
- *      $(Element).css(json)                              // 设置全部元素多个样式                         "[可继续链式]"
- *      $(Element).css(key, value)                        // 设置全部元素单个样式                         "[可继续链式]"
- *      $(Element).css(key)                               // 获取第一个元素的样式                         "[不续链式]"
- * 
- * 
- * 静态API:
- *      .removeClass(el, ClassName)                      // 移除元素单个类
- *      .addClass(el, ClassName)                         // 添加元素单个类
- *      .toggleClass(el, ClassName)                      // 切换元素单个类
- *      .classList(el, ClassName)                        // 返回类列表
- *      .hasClass(el, ClassName)                         // 返回类是否存在
- *      .isElement(el)                                   // 返回是否为HTML元素
- *      .typeOf(value)                                   // 返回值的类型，或者对象名称
- * 
- */
+
 ;(function(doc) {
 
   // 观察者
